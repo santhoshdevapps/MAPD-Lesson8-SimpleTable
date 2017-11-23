20e9729b21ea72304ecdf8c47bb5c9e090bf6de5
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2017-11-22 21:39:46 -0500

    completed tableview

--- MAPD-Lesson8-SimpleTable/ViewController.swift
+++ MAPD-Lesson8-SimpleTable/ViewController.swift
@@ -15,15 +15,71 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        
         if(cell == nil){
         cell = UITableViewCell(
         style: UITableViewCellStyle.default,reuseIdentifier:
         simpleTableIdentifier)
         
     }
-    cell?.textLabel?.text = movies[indexPath.row]
+        
+        let image = UIImage(named: "star")
+        cell?.imageView?.image = image
+        let highlightedImage = UIImage(named: "star2")
+        cell?.imageView?.highlightedImage = highlightedImage
+        
+        if (indexPath.row < 4) {
+            cell?.detailTextLabel?.text = "Superhero Movie"
+        } else {
+            cell?.detailTextLabel?.text = "Sci-Fi Movie"
+        }
+        
+        cell?.textLabel?.text = movies[indexPath.row]
+        return cell!
+    }
     
-    return cell!
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int
+    {
+        return indexPath.row % 4
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return indexPath.row == 0 ? nil : indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowValue = movies[indexPath.row]
+        let message = "You selected \(rowValue)"
+        
+        let controller = UIAlertController(
+            title: "Row Selected",
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Yes I Did!",
+            style: .default,
+            handler: nil)
+        
+        controller.addAction(action)
+        present(controller, animated: true, completion: nil)
+    }
 }
 
-} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
