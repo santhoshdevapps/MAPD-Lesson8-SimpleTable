0fb980e5154afa87dad3907a099b5eebe28a879a
santhosh damodharan <santhoshdamodharan@santhoshs-MacBook-Air.local>
2017-11-22 19:47:58 -0500

    created a simple table view structure

--- MAPD-Lesson8-SimpleTable/ViewController.swift
+++ MAPD-Lesson8-SimpleTable/ViewController.swift
@@ -1,25 +1,29 @@
-//
-//  ViewController.swift
-//  MAPD-Lesson8-SimpleTable
-//
-//  Created by santhosh damodharan on 2017-11-22.
-//  Copyright © 2017 santhosh damodharan. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    private let movies = ["Thor", "Blade Runner", "Batman vs Superman", "Justice League", "wonderwoman", "Dark Tower", "Star Wars"]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if(cell == nil){
+        cell = UITableViewCell(
+        style: UITableViewCellStyle.default,reuseIdentifier:
+        simpleTableIdentifier)
+        
+    }
+    cell?.textLabel?.text = movies[indexPath.row]
+    
+    return cell!
 }
 
+} 
